475517db7c751d25b2f02895d4d11b84d26a72b7
LUPUS001 <antoniodaw23@gmail.com>
2026-05-12 11:40:50 +0200

    3.3 - Validación de formularios

--- src/Entity/Contacto.php
+++ src/Entity/Contacto.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\ContactoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: ContactoRepository::class)]
 class Contacto
@@ -14,12 +15,16 @@ class Contacto
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
+    #[Assert\NotBlank] // Indicamos que los campos 'nombre, telefono e email' no pueden estar vacíos
     private ?string $nombre = null;
 
     #[ORM\Column(length: 15)]
+    #[Assert\NotBlank]
     private ?string $telefono = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type:"string", length: 255)] // (*1) para que sea un email válido (que solo sea texto y no hacer operaciones con él porque el correo tenga números) ponemos el type 'string'
+    #[Assert\NotBlank]
+    #[Assert\Email(message: "El email {{ value }} no es válido")] // (*2) creamos un mensaje de error personalizado que saldrá si el email no es válido
     private ?string $email = null;
 
     #[ORM\ManyToOne(targetEntity:'Provincia', cascade:['persist'])]
@@ -41,7 +46,7 @@ class Contacto
         return $this->nombre;
     }
 
-    public function setNombre(string $nombre): static
+    public function setNombre(?string $nombre): static
     {
         $this->nombre = $nombre;
 
@@ -53,7 +58,7 @@ class Contacto
         return $this->telefono;
     }
 
-    public function setTelefono(string $telefono): static
+    public function setTelefono(?string $telefono): static
     {
         $this->telefono = $telefono;
 
@@ -65,7 +70,7 @@ class Contacto
         return $this->email;
     }
 
-    public function setEmail(string $email): static
+    public function setEmail(?string $email): static
     {
         $this->email = $email;
 
@@ -89,3 +94,26 @@ class Contacto
         return $this;
     }
 }
+
+/*
+(*1)
+    usamos type:string y NO type:email porque en la base de datos el email es una cadena de texto, dentro
+    de la base de datos no hay un tipo "EMAIL" sino que se guardará como "VARCHAR" (string en Doctrine)
+*/
+
+/*
+(*2)
+    Este Assert no lo podremos comprobar porque el navegador ya detectará el campo email (gracias a EmailType::class en ContactoType) 
+    y no nos dejará subirlo si el email no es válido, así que si queremos comprobar este Assert, tenemos que "engañar al navegador":
+
+    - En el inspector (F12) casbia el tipo del campo email a "text y entonces envíar el formulario.
+    - Haciendo esto, será el servidor PHP que hemos configurado nosotros el que detecte el error y muestre el mensaje de Assert.
+
+    Aunque, todo sea dicho, simplemente con poner '@' ya pasamos la primera barrera que es el navegador y nos deja subir el formulario,
+    solo que en lugar de poner correo@x.com ponemos correo@x sin el .com,  .es, ... ya nos dejará subir el formulario, por lo que aunque 
+    parezca que no y se válide de forma automática, el nuevo Assert mete una segunda capa de seguridad que realmente es necesaria
+
+    1. Si no tuvieramos ni EmailType::class ni Assert #[Assert\Email()..] podríamos subir cualquier cosa como correo
+    2. Solo con EmailType::class si que tiene que seguir un formato correo pero aún tiene una vulnerabilidad (podemos subir a@a como correo válido)
+    3. Si añadimos #[Assert\Email()..] junto a EmailType si que tendrá que ser un correo "perfecto" y adecuado.
+*/
